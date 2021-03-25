82d8c97188eff6ccc008709de58e6afdb4af6021
Diego Cely <diegoacelyb@gmail.com>
2021-03-25 15:32:55 -0500

    Spring Boot java project has been created

---
+++ src/main/java/co/com/diegoalceb/JavaConDockerApplication.java
@@ -0,0 +1,13 @@
+package co.com.diegoalceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConDockerApplication.class, args);
+	}
+
+}
